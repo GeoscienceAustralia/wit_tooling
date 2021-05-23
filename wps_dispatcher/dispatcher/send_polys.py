e97c3f5ab1b415aaad38d837fc0e0e452c66bb78
Tisham Dhar <whatnickd@gmail.com>
2021-05-23 22:18:56 +1000

    #8 Skeleton of dispatcher container
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

---
+++ wps_dispatcher/dispatcher/send_polys.py
@@ -0,0 +1,2 @@
+"""WPS client via click designed to send a subset of polygons in a collection to WPS backend
+"""
---
+++ wps_dispatcher/tests/test_wps_client.py
@@ -0,0 +1,2 @@
+"""Test reading a polygon and sending a single WPS request to termination
+"""
